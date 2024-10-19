1634571a535fcc4ac79b698130c17a4ba6e02010
rohancorbyn <17966183+rohan-corbyn@users.noreply.github.com>
2024-10-19 11:46:13 +0100

    Auto formatted script.js
    project completed.

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ function playGame(e) {
     const computerChoice = choices.indexOf(computerChoiceString);
     document.querySelector("#player").setAttribute("src", "./img/" + playerChoiceString + ".png");
     document.querySelector("#computer").setAttribute("src", "./img/" + computerChoiceString + ".png");
-    
+
     if (playerChoice === computerChoice) {
         document.querySelector("#result").setAttribute("src", "./img/result-draw.png");
-    } else if (playerChoice === computerChoice +1 || playerChoice === 0 && computerChoice == 2) {
+    } else if (playerChoice === computerChoice + 1 || playerChoice === 0 && computerChoice == 2) {
         document.querySelector("#result").setAttribute("src", "./img/result-won.png");
     } else {
         document.querySelector("#result").setAttribute("src", "./img/result-lost.png");
